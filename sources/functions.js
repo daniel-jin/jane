2300d87d0a4c58464333b8924c5c26778188f1f3
daniel-jin <daniel.h.jin@gmail.com>
2018-04-04 16:38:45 -0600

    Finishing up signup function

--- sources/functions.js
+++ sources/functions.js
@@ -1,6 +1,12 @@
 const selectors = require('./selectors')
 const data = require('./data')
 
+/**
+ * Clears an input field, sets an assigned value, and verifies the value.
+ * @param {object} browser the Nightwatch testing object
+ * @param {string} selector the selectors on the page
+ * @param {string} data input data for the field
+ */
 let setInputValue = (browser, selector, data) => {
     browser
         .clearValue(selector)
@@ -8,6 +14,10 @@ let setInputValue = (browser, selector, data) => {
         .verify.value(selector, data)
 }
 
+/**
+ * 
+ * @param {*} browser 
+ */
 let signUpValid = (browser) => {
     browser
         .click(selectors["login/signup"].homepageLogin)
@@ -15,10 +25,17 @@ let signUpValid = (browser) => {
     browser
         .click(selectors["login/signup"].signupLink)
         .waitForElementVisible(selectors["login/signup"].signUpButton, 3000)
-    setInputValue(browser, selectors["login/signup"].nameInput, "")
-    setInputValue(browser, selectors["login/signup"].emailInput, "")
-    setInputValue(browser, selectors["login/signup"].passwordInput, "")
-    
+
+    setInputValue(browser, selectors["login/signup"].nameInput, data.signUp.fullName)
+    setInputValue(browser, selectors["login/signup"].emailInput, data.signUp.email)
+    setInputValue(browser, selectors["login/signup"].passwordInput, data.signUp.password)
+
+    browser
+        .click(selectors["login/signup"].signUpButton)
+        .waitForElementVisible(selectors["login/signup"].homepageLogin, 3000)
+    browser
+        .expect.element(selectors["login/signup"].homepageLogin).text.to.equal("Dan")
+
     
 
 
