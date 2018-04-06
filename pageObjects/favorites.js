c8562d6384ef15629bfe5b6fefa41d6313909ee6
daniel-jin <daniel.h.jin@gmail.com>
2018-04-06 14:18:48 -0600

    Started page objects file for favorites

---
+++ pageObjects/favorites.js
@@ -0,0 +1,8 @@
+module.exports = {
+    url: 'https://jane.com/favorites',
+
+    elements: {
+        homepageLogin: 'span[class="icon-title__IconText-flJJid bwzEim text__Text-iEUpza htVFSd"]',
+        favoritesButton: 'i[data-testid="favorites-btn"]',
+    }
+}
